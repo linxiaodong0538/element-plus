ba6db8e480fb0b302cfd4dcbc8fff9414575eb8e
linxin <34860841+linxiaodong0538@users.noreply.github.com>
2022-09-14 09:04:58 +0800

    Update basic.vue

--- docs/examples/button/basic.vue
+++ docs/examples/button/basic.vue
@@ -19,7 +19,7 @@
 
   <el-row class="mb-4">
     <el-button round>Round</el-button>
-    <el-button type="primary" round>Primary</el-button>
+    <el-button type="primary" round>Primary1111111111111</el-button>
     <el-button type="success" round>Success</el-button>
     <el-button type="info" round>Info</el-button>
     <el-button type="warning" round>Warning</el-button>
